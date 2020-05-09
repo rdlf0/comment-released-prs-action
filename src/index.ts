3c8cc3ea48878d48fa352de354a3bb85136c85ea
Radoslav Stoyanov <r.stoyanov@outlook.com>
2020-05-09 08:07:23 -0400

    Testtt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { setFailed } from "@actions/core"
 import { context } from "@actions/github"
-import { WebhookPayloadReleaseRelease as Release } from "@octokit/webhooks"
+// import { WebhookPayloadReleaseRelease as Release } from "@octokit/webhooks"
+import * as webhooks from "@octokit/webhooks"
 
 async function run(): Promise<void> {
     try {
@@ -21,15 +22,16 @@ async function run(): Promise<void> {
         console.log("HELLLO!");
         console.log(context.payload);
 
-        const release: Release = context.payload as Release;
+        // const release: Release = context.payload as Release;
+        const release: webhooks.WebhookPayloadRelease = context.payload as webhooks.WebhookPayloadRelease;
 
-        console.log(`Release ID=${release.id}, tag=${release.tag_name}`);
-        console.log(release.id.toString());
-        console.log(release.author.login);
-        console.log(release.tag_name);
-        console.log(release.assets_url);
-        console.log("Here's the whole payload:");
-        console.log(release);
+        console.log(`Release ID=${release.release.id}, tag=${release.release.tag_name}`);
+        // console.log(release.id.toString());
+        // console.log(release.author.login);
+        // console.log(release.tag_name);
+        // console.log(release.assets_url);
+        // console.log("Here's the whole payload:");
+        // console.log(release);
 
         // core.setOutput("pr-ids", "Some IDs will come here")
     } catch (error) {
